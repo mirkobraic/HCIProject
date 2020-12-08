20b0103bb71ab21f4671daff59ce71c06c86b4ff
mirkobraic <mirko.braic@gmail.com>
2020-12-08 23:32:24 +0100

    Add navigation pages

---
+++ src/pages/blog.js
@@ -0,0 +1,9 @@
+import React from "react"
+import HeaderFooterLayout from "../layouts/headerFooter"
+
+const BlogPage = () => (
+    <HeaderFooterLayout activeTab="Blog">
+    </HeaderFooterLayout>
+)
+
+export default BlogPage
