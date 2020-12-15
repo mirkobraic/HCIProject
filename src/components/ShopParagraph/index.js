722a40665938371240268f44d40567937882393c
dcvita01 <dcvita01@fesb.hr>
2020-12-15 16:49:05 +0100

    Fixed tipo in ShopParagraph

---
+++ src/components/ShopParagraph/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from './style.module.css'
+ 
+const ShopParagraph = () => <section className={styles.shopParagraph}>
+ 
+</section> 
+ 
+
+export default ShopParagraph
