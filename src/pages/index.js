aef8e6376ff567a02c9ded440662616b56e3bb25
dcvita01 <dcvita01@fesb.hr>
2020-12-15 16:05:18 +0100

    Created title, font changed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
  
 import HeaderFooterLayout from "../layouts/headerFooter"
- 
+import TitlePage from '../modules/TitlePage'
+
 const IndexPage = () => (
   <HeaderFooterLayout activeTab="Home">
+    <TitlePage />
   </HeaderFooterLayout>
 )
  
