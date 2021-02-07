30838da4b70960bc8719646692629c59a7a0d105
dcvita01 <dcvita01@fesb.hr>
2021-02-07 11:55:41 +0100

    contact page ContactSeparator

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,11 +2,13 @@ import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import TitlePage from '../modules/TitlePage'
 import ContactForm from '../modules/ContactForm'
+import ContactSeperator from '../modules/ContactSeperator'
 
 
 const ContactPage = () => (
     <HeaderFooterLayout activeTab="Contact">
         <TitlePage />
+        <ContactSeperator />
         <ContactForm />
     </HeaderFooterLayout>
 )
