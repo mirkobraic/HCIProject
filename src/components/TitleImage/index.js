d581cfb8c5ec3ad37ce796b1a29e4c2d7f78a7bc
dcvita01 <dcvita01@fesb.hr>
2021-02-07 09:58:49 +0100

    TitleImage imported using GraphQL

--- src/components/TitleImage/index.js
+++ src/components/TitleImage/index.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react'
  
 import styles from './style.module.css'
@@ -10,4 +11,40 @@ const TitleImage = () => (
     </section>
 )
  
+export default TitleImage
+
+*/
+
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
+
+import styles from "./style.module.css"
+
+const TitleImage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      desktop: file(relativePath: { eq: "snow.jpg" }) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <BackgroundImage
+      Tag="section"
+      className={styles.titleImage}
+      fluid={data.desktop.childImageSharp.fluid}
+      backgroundColor={`#040e18`}
+    >
+      <div>
+        <h1>EXTREME SPORTS</h1>
+      </div>
+    </BackgroundImage>
+  )
+}
+
 export default TitleImage
