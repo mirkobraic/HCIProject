293f15f05b4659d9f260b1ab54516bab13636d8d
dcvita01 <dcvita01@fesb.hr>
2021-02-08 11:57:28 +0100

    TravelSeparator added

---
+++ src/modules/Travel/TravelSeparator/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+ 
+import styles from './style.module.css'
+ 
+const TravelSeparator = () => (
+    <section className={styles.separator}>
+        <h2>TRAVEL</h2>
+    </section>
+)
+ 
+export default TravelSeparator
--- src/pages/travel.js
+++ src/pages/travel.js
@@ -1,10 +1,12 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import TitlePage from '../modules/TitlePage'
+import TravelSeparator from '../modules/Travel/TravelSeparator'
 
 const TravelPage = () => (
     <HeaderFooterLayout activeTab="Travel">
         <TitlePage />
+        <TravelSeparator />
     </HeaderFooterLayout>
 )
 
