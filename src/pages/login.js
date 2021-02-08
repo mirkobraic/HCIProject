6aedf7d8f4111c8ba864ad2c6604ef8fe967d19e
dcvita01 <dcvita01@fesb.hr>
2021-02-08 12:02:07 +0100

    LoginSeparator added

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,12 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import TitlePage from '../modules/TitlePage'
+import LoginSeparator from '../modules/Login/LoginSeparator'
 
 const LoginPage = () => (
     <HeaderFooterLayout activeTab="Login">
         <TitlePage />
+        <LoginSeparator />
     </HeaderFooterLayout>
 )
 
