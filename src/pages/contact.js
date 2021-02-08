ffb3d53c83c284ae62eaa386569db2b350ddb03a
dcvita01 <dcvita01@fesb.hr>
2021-02-08 11:54:58 +0100

    ShopSeparator added

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,8 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import TitlePage from '../modules/TitlePage'
-import ContactForm from '../modules/ContactForm'
-import ContactSeperator from '../modules/ContactSeperator'
+import ContactForm from '../modules/Contact/ContactForm'
+import ContactSeperator from '../modules/Contact/ContactSeperator'
 
 
 const ContactPage = () => (
--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,10 +1,12 @@
 import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import TitlePage from '../modules/TitlePage'
+import ShopSeparator from '../modules/Shop/ShopSeparator'
 
 const ShopPage = () => (
     <HeaderFooterLayout activeTab="Shop">
         <TitlePage />
+        <ShopSeparator />
     </HeaderFooterLayout>
 )
 
