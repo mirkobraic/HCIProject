1ed426d9686f856790f059374c760f5ec5033be4
dcvita01 <dcvita01@fesb.hr>
2021-02-09 12:03:49 +0100

    Login form added

--- src/components/Header/Navigation/index.js
+++ src/components/Header/Navigation/index.js
@@ -6,7 +6,6 @@ import styles from './style.module.css'
 const Navigation = ({ activeTab }) => (
     <section className={styles.navigation}>
         <NavigationTabs activeTab={activeTab} />
-        {/* TODO: add cart */}
     </section>
 )
  
--- src/pages/login.js
+++ src/pages/login.js
@@ -2,11 +2,14 @@ import React from "react"
 import HeaderFooterLayout from "../layouts/headerFooter"
 import TitlePage from '../modules/TitlePage'
 import LoginSeparator from '../modules/Login/LoginSeparator'
+import Login from '../modules/Login/Login'
+
 
 const LoginPage = () => (
     <HeaderFooterLayout activeTab="Login">
         <TitlePage />
         <LoginSeparator />
+        <Login />
     </HeaderFooterLayout>
 )
 
