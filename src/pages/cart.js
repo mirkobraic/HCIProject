a0314f662dac05fe876f3100c947405084e1e573
dcvita01 <dcvita01@fesb.hr>
2021-02-12 13:03:55 +0100

    Content added to shopping cart

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -4,7 +4,23 @@ import { Link } from 'gatsby'
 import styles from './cart.module.css'
 
 const CartPage = () => (
-	<selection className={styles.pageContent}>
+	<div className={styles.pageContent}>
+		<h1>Shopping Cart</h1>
+
+		<p>PRODUCTS...</p>
+		
+		<span>Subtotal</span>
+		<span> 17.99</span>
+		<br/>
+
+		<span>Shipping costs</span>
+		<span> 3.99</span>
+		<br/>
+
+		<span className={styles.total}>Total</span>
+		<span className={styles.total}> 21.98</span>
+		<br/>
+
 		<Link to={'/'}>
 			<bottun className={styles.homeButton}>Proceed to checkout</bottun>
 		</Link>
@@ -12,7 +28,7 @@ const CartPage = () => (
 		<Link to={'/shop'}>
 			<bottun className={styles.shopButton}>Continue shopping</bottun>
 		</Link>
-	</selection>
+	</div>
 )
 
 export default CartPage
