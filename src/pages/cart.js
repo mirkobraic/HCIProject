444fafa63402617e604af7ade3222add6ab491bf
dcvita01 <dcvita01@fesb.hr>
2021-02-12 16:32:07 +0100

    NavBar shadow added

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -5,26 +5,31 @@ import styles from './cart.module.css'
 
 const CartPage = () => (
 	<div className={styles.pageContent}>
-		<h1>Shopping Cart</h1>
+		<h1 className={styles.pageTitle}>Shopping Cart</h1>
+		
+		{/*
+		TODO:
+		product component
+		*/}
 
 		<p>PRODUCT 1</p>
-		<hr/>
+		<hr className={styles.productSeparator}/>
 		<p>PRODUCT 2</p>
-		<hr/>
+		<hr className={styles.productSeparator}/>
 
-		<p>
+		<p className={styles.sum}>
 			<span>Subtotal</span>
 			<span> 17.99</span>
 		</p>
 
-		<p>
+		<p className={styles.sum}>
 			<span>Shipping costs</span>
 			<span> 3.99</span>
 		</p>
 
-		<p className={styles.total}>
-			<span>Total</span>
-			<span> 21.98</span>
+		<p className={styles.sum}>
+			<span className={styles.total}>Total</span>
+			<span className={styles.total}> 21.98</span>
 		</p>
 
 		<Link to={'/'}>
