7ba7e7809e0222dab6aa9fac0cc1ebb8deb69c68
dcvita01 <dcvita01@fesb.hr>
2021-02-14 13:53:20 +0100

    Travel item design edited

--- src/layouts/blog.js
+++ src/layouts/blog.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { renderRichText } from "gatsby-source-contentful/rich-text"
 import Img from 'gatsby-image'
 import {Link} from 'gatsby'
+import ReactMarkdown from 'react-markdown'
  
 import styles from './blog.module.css'
 import HeaderFooterLayout from '../layouts/headerFooter'
@@ -19,9 +20,7 @@ const BlogPost = ({ pageContext }) => {
           </h2>
         </header>
         <Img fixed={image.fixed} />
-        <article>
-        {blogText.internal.content}
-        </article>
+        <ReactMarkdown children={blogText.internal.content} />
       </main>
     </HeaderFooterLayout>
   )
--- src/layouts/travel.js
+++ src/layouts/travel.js
@@ -2,27 +2,23 @@ import React from 'react'
 import { renderRichText } from "gatsby-source-contentful/rich-text"
 import Img from 'gatsby-image'
 import {Link} from 'gatsby'
+import ReactMarkdown from 'react-markdown'
  
 import styles from './travel.module.css'
 import HeaderFooterLayout from '../layouts/headerFooter'
  
 const TravelPost = ({ pageContext }) => {
-  const { longText, title, image, price, time} = pageContext
+  const { longText, title, image, price} = pageContext
  
   return (
     <HeaderFooterLayout>
       <main className={styles.container}>
-        <header>
-          <h2>
-            {title}
-          </h2>
-        </header>
-        <Img fixed={image.fixed} />
-        <article>
-            {longText.internal.content}
-        </article>
-        <span>{price}</span>
-        <span>{time}</span>
+        <p className={styles.textHalf}>
+            <h2>{title}</h2>
+            <ReactMarkdown children={longText.internal.content} />
+            <bottun className={styles.priceBottun}>Book now: {price}</bottun>
+        </p>
+        <Img className={styles.travelImage} fixed={image.fixed} />
       </main>
     </HeaderFooterLayout>
   )
