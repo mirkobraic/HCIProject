7b4598d1226c3ef28fffe3cf5e7719665e735f1b
dcvita01 <dcvita01@fesb.hr>
2021-02-14 11:47:05 +0100

    Home page content linked to other pages

--- src/modules/Home/Blog/index.js
+++ src/modules/Home/Blog/index.js
@@ -1,11 +1,14 @@
 import React from "react"
+import { Link } from 'gatsby'
 
 import styles from './style.module.css'
 import BlogsContainer from '../../Blog/BlogsContainer'
 
 const BlogContent = () => (
 	<section className={styles.blogContent}>
-		<h1>Blog >></h1>
+		<Link to={'/blog'}>
+			<h1>Blog >></h1>
+		</Link>
 		<BlogsContainer />
 	</section>)
 
--- src/modules/Home/Shop/index.js
+++ src/modules/Home/Shop/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 import styles from './style.module.css'
 
@@ -10,14 +11,16 @@ import CategoryMore from '../../../components/Images/CategoryMore'
 
 
 const ShopContent = () => (
-  <section className={styles.shopContent}>
-  <h1>SHOP >></h1>
-  <div>
-    <ShopCategory image={<Category1/>} title="Boats and paddle boats"/>
-    <ShopCategory image={<Category2/>} title="Equipment"/>
-    <ShopCategory image={<Category3/>} title="Clothing"/>
-    <ShopCategory image={<CategoryMore/>} title="Show more categories..."/>
-  </div>
- </section>)
+	<section className={styles.shopContent}>
+		<Link to={'/shop'}>
+			<h1>Shop >></h1>
+		</Link>
+		<div>
+			<ShopCategory image={<Category1/>} title="Boats and paddle boats"/>
+			<ShopCategory image={<Category2/>} title="Equipment"/>
+			<ShopCategory image={<Category3/>} title="Clothing"/>
+			<ShopCategory image={<CategoryMore/>} title="Show more categories..."/>
+		</div>
+	</section>)
 
 export default ShopContent
