ae0922bbf960f1873fadfc37fd5896b721b21785
mirkobraic <mirko.braic@gmail.com>
2021-02-14 23:34:53 +0100

    Add produc categories

--- src/constants/const.js
+++ src/constants/const.js
@@ -5,3 +5,10 @@ export const navs = [
     {tab: 'Blog', to: '/blog'},
     {tab: 'Contact', to: '/contact'}
 ]
+
+export const categories = [
+    {name: "Snowboarding", productTypes: ["Boards", "Bindings", "Boots", "Helmets", "Goggles", "Jackets", "Pants", "Gloves"], brands: ["Burton", "Capita", "DC", "GNU", "Jones", "Lib Tech", "Nitro"]}, 
+    {name: "Whitewater Kayaking", productTypes: ["Kayaks", "Paddles", "Spraydecks", "Buoyancy aids", "Helmets", "Clothing"], brands: ["Dagger", "Pyranha", "Jackson", "Palm", "Astral", "Kokatat"]},
+    {name: "Mountain Biking", productTypes: ["Bikes", "Equipment", "Parts"], brands: ["Specialized", "Trek Bikes", "Santa Cruz", "Giant Bicycles"]},
+    {name: "Surfing", productTypes: ["Kiteboard", "Windsurf", "Wakeboard", "SUP", "Wetsuits", "Shoes"], brands: ["Crazyfly", "Quicksliver", "Ion"]}
+]
