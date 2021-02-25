f1199513b4a66d8d9c7c66103fae53461013aaf6
mirkobraic <mirko.braic@gmail.com>
2021-02-25 01:57:27 +0100

    Improve sort

--- src/components/SortTravel/index.js
+++ src/components/SortTravel/index.js
@@ -6,26 +6,26 @@ import { sortCriteriaTravel } from '../../constants/const'
 
 const SortTravel = ({ handleChange }) => {
     return (
-        <div className={styles.sort}>
-            <fieldset className={styles.sortCard}>
-                <label htmlFor="sort" className={styles.sortLabel}>
-                    Sort by:&nbsp;
-                </label>
-                <select
-                  name="sort"
-                  className={styles.sortInput}
-                  onChange={handleChange}
-                  >
+      <div className={styles.sort}>
+      <fieldset className={styles.sortCard}>
+          <label htmlFor="sort" className={styles.sortLabel}>
+              Sort by:&nbsp;
+          </label>
+          <select
+            name="sort"
+            className={styles.sortInput}
+            onChange={handleChange}
+            >
 
-                  <option value={''}>Price</option>
-                  {
-                    sortCriteriaTravel.map(sort => (
-                      <option key={sort.name} value={sort.name}>{sort.name}</option>
-                    ))
-                  }
-                </select>
-            </fieldset>
-        </div>
+            <option value={''}>Rating</option>
+            {
+              sortCriteriaTravel.map(sort => (
+                <option key={sort.name} value={sort.name}>{sort.name}</option>
+              ))
+            }
+          </select>
+      </fieldset>
+  </div>
     )
 }
 
